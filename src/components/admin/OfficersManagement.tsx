16de260e9041b83fc70fbbf6098d79fd51109354
rbyers87 <35844687+rbyers87@users.noreply.github.com>
2025-10-21 15:41:44 -0500

    Update OfficersManagement.tsx

--- src/components/admin/OfficersManagement.tsx
+++ src/components/admin/OfficersManagement.tsx
@@ -21,7 +21,7 @@ interface WeeklyScheduleProps {
   isAdminOrSupervisor: boolean;
 }
 
-export const WeeklySchedule = ({ userId, isAdminOrSupervisor }: WeeklyScheduleProps) => {
+export const OfficersManagement = ({ userId, isAdminOrSupervisor }: WeeklyScheduleProps) => {
   const [dialogOpen, setDialogOpen] = useState(false);
   const [ptoDialogOpen, setPtoDialogOpen] = useState(false);
   const [selectedOfficerId, setSelectedOfficerId] = useState<string>(userId);
