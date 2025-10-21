027af209cac7544ed68d4a6338c126e85efbb13a
rbyers87 <35844687+rbyers87@users.noreply.github.com>
2025-10-21 17:50:29 -0500

    Update OfficersManagement.tsx

--- src/components/schedule/OfficersManagement.tsx
+++ src/components/schedule/OfficersManagement.tsx
@@ -1,3 +1,4 @@
+//this is the officer tab for officer weekly and monthly schedules
 import { useState, useEffect } from "react";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { supabase } from "@/integrations/supabase/client";
