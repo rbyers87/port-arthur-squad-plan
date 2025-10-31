b509bb79ad575bc84efab9cc3081fec515d9c8a0
rbyers87 <35844687+rbyers87@users.noreply.github.com>
2025-10-31 16:53:37 -0500

    Update Dashboard.tsx

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -509,7 +509,7 @@ const Dashboard = () => {
       <TabsTrigger value="vacancies">Vacancies</TabsTrigger>
       <TabsTrigger value="staff">Staff</TabsTrigger>
       <TabsTrigger value="requests">Time Off</TabsTrigger>
-      <TabsTrigger value="pto">PTO</TabsTrigger>
+      //<TabsTrigger value="pto">PTO</TabsTrigger>
     </TabsList>
 
     <TabsContent value="daily" className="space-y-6">
